3df1d2b32a1aab4782a2714ca27a83ef9af17a81
tom-bjss <92319736+tom-bjss@users.noreply.github.com>
2022-01-07 13:33:02 +0000

    feat(cb2-1051): added wrapper for event and interface created for export format (#4)

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import 'source-map-support/register';
 import { DynamoDBStreamEvent, Context, Callback } from 'aws-lambda';
+import { getSecret } from './utils/filterUtils';
+import { TestActivity } from './utils/testActivity';
 import logger from './observability/logger';
 
 const {
@@ -11,15 +13,18 @@ logger.debug(
   `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_STAGE}'\n region: '${AWS_REGION}'\n\n`,
 );
 
-logger.debug(
-  `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_STAGE}'\n region: '${AWS_REGION}'\n\n`,
-);
-
-const handler = (event: DynamoDBStreamEvent, _context: Context, callback: Callback) => {
+const handler = async (event: DynamoDBStreamEvent, _context: Context, callback: Callback) => {
   try {
     logger.debug(`Function triggered with '${JSON.stringify(event)}'.`);
+    const secrets: string[] = await getSecret(process.env.SECRET_NAME);
 
-    // TODO: Format and Send test results
+    event.Records.forEach((record) => {
+      // TODO: format record to Test Activity type
+      let testActivity: TestActivity;
+      if (secrets.includes(testActivity.testStationPNumber)) {
+        // Send test result
+      }
+    });
 
     logger.info('Data processed successfully.');
     callback(null, 'Data processed successfully.');
