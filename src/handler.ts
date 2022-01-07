9261e5b5b372ee3eb4e7cfec375412410b227114
tom-bjss <92319736+tom-bjss@users.noreply.github.com>
2022-01-07 12:27:49 +0000

    CB2-1053: Create the ingestion Lambda (#3)
    * feat(cb2-1053): added boilerplate code from wms lambda
    
    * feat(cb2-1053): added config to skip lib check

--- src/handler.ts
+++ src/handler.ts
@@ -1,40 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import 'source-map-support/register';
-import serverless from 'serverless-http';
-import { Context, APIGatewayEvent, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
-
-import { app } from './infrastructure/api';
+import { DynamoDBStreamEvent, Context, Callback } from 'aws-lambda';
+import logger from './observability/logger';
 
 const {
-  NODE_ENV, SERVICE, AWS_PROVIDER_REGION, AWS_PROVIDER_STAGE,
+  NODE_ENV, SERVICE, AWS_REGION, AWS_STAGE,
 } = process.env;
 
-console.log(
-  `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_PROVIDER_STAGE}'\n region: '${AWS_PROVIDER_REGION}'\n\n`,
+logger.debug(
+  `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_STAGE}'\n region: '${AWS_REGION}'\n\n`,
 );
 
-const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyStructuredResultV2> => {
-  console.log('event');
-  console.log(JSON.stringify(event, null, 2));
-  /**
-   * Proxied requests from AWS are intercepted and handled by our express Router -> <stage>/<*> where stage is your branch
-   * Our app allows proxies requests until /template path is reached
-   * Express will then handle/call a Controller to call the appropriate functions
-   * Our app allows the following paths -> <stage>/<**>/<*>/template/<service_endpoints>
-   *
-   * |===========================================================|
-   * | Example of event object received                          |
-   * |-----------------------------------------------------------|
-   * | ...,                                                      |
-   * | "httpMethod": "POST",                                     |
-   * | "path": "/proxiedstuff/svc-svc-template/1/something",     |
-   * | "pathParameters": {                                       |
-   * |    "proxy": "proxiedstuff/svc-svc-template/1/something"   |
-   * |  },                                                       |
-   * |  ...                                                      |
-   * |-----------------------------------------------------------|
-   *
-   */
-  return serverless(app)(event, context);
-};
+logger.debug(
+  `\nRunning Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_STAGE}'\n region: '${AWS_REGION}'\n\n`,
+);
 
+const handler = (event: DynamoDBStreamEvent, _context: Context, callback: Callback) => {
+  try {
+    logger.debug(`Function triggered with '${JSON.stringify(event)}'.`);
+
+    // TODO: Format and Send test results
+
+    logger.info('Data processed successfully.');
+    callback(null, 'Data processed successfully.');
+  } catch (error) {
+    logger.info('Data processed unsuccessfully.');
+    logger.error('', error);
+    callback(new Error('Data processed unsuccessfully.'));
+  }
+};
 export { handler };
