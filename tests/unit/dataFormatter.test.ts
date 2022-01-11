f73c09abb9a3c2fafd97b5e57c9e92dbd219183f
Aaron Downing <aaron.downing@bjss.com>
2022-01-11 10:56:38 +0000

    feat(cb2-1051): add unit tests for dataFormatter

--- tests/unit/dataFormatter.test.ts
+++ tests/unit/dataFormatter.test.ts
@@ -5,19 +5,18 @@
 import { DynamoDBRecord } from 'aws-lambda';
 import { formatDynamoData } from '../../src/utils/dataFormatter';
 import { TestActivity } from '../../src/utils/testActivity';
-import dynamoEvent from './data/dynamoEvent.json';
+import dynamoEventWOCert from './data/dynamoEventWithoutCert.json';
+import dynamoEventWCert from './data/dynamoEventWithCert.json';
+import dynamoEventMultipleTests from './data/dynamoEventMultipleTestTypes.json';
 
 describe('DataFormatter', () => {
   let DYNAMO_DATA: DynamoDBRecord;
   let TEST_AVTIVITY: TestActivity;
 
-  beforeEach(() => {
-    DYNAMO_DATA = dynamoEvent as DynamoDBRecord;
-    TEST_AVTIVITY = formatDynamoData(DYNAMO_DATA);
-  });
-
   describe('formatDynamoData', () => {
-    it('should return a correctly formatted test activity with latest test result information', () => {
+    it(`GIVEN test result WITHOUT certificate number issued WHEN data is formatted into event THEN event doesn't have certificate number`, () => {
+      DYNAMO_DATA = dynamoEventWOCert as DynamoDBRecord;
+      TEST_AVTIVITY = formatDynamoData(DYNAMO_DATA);
       const EXPECTED_TEST_AVTIVITY: TestActivity = {
         noOfAxles: 2,
         testTypeStartTimestamp: '2019-01-14T10:36:33.987Z',
@@ -27,7 +26,28 @@ describe('DataFormatter', () => {
         vin: 'XMGDE02FS0H012303',
         vrm: 'JY58FPP',
         testStationPNumber: 'P99006',
-        testResult: 'pass',
+        testResult: 'fail',
+        testTypeName: 'Annual test',
+        vehicleType: 'psv',
+        testerName: 'Dorel',
+        testerStaffId: '2',
+        testResultId: '9',
+      };
+      expect(TEST_AVTIVITY).toEqual(EXPECTED_TEST_AVTIVITY);
+    });
+    it(`GIVEN test result WITH certificate number issued WHEN data is formatted into event THEN event has certificate number`, () => {
+      DYNAMO_DATA = dynamoEventWCert as DynamoDBRecord;
+      TEST_AVTIVITY = formatDynamoData(DYNAMO_DATA);
+      const EXPECTED_TEST_AVTIVITY: TestActivity = {
+        noOfAxles: 2,
+        testTypeStartTimestamp: '2019-01-14T10:36:33.987Z',
+        testTypeEndTimestamp: '2019-01-14T10:36:33.987Z',
+        testStationType: 'gvts',
+        testCode: 'aas',
+        vin: 'XMGDE02FS0H012303',
+        vrm: 'JY58FPP',
+        testStationPNumber: 'P99006',
+        testResult: 'fail',
         certificateNumber: '1234',
         testTypeName: 'Annual test',
         vehicleType: 'psv',
@@ -37,5 +57,27 @@ describe('DataFormatter', () => {
       };
       expect(TEST_AVTIVITY).toEqual(EXPECTED_TEST_AVTIVITY);
     });
+    it(`GIVEN multiple test types WHEN data is formated into event THEN expect latest test type in event`, () => {
+      DYNAMO_DATA = dynamoEventMultipleTests as DynamoDBRecord;
+      TEST_AVTIVITY = formatDynamoData(DYNAMO_DATA);
+      const EXPECTED_TEST_AVTIVITY: TestActivity = {
+        noOfAxles: 2,
+        testTypeStartTimestamp: '2019-01-14T10:36:33.987Z',
+        testTypeEndTimestamp: '2019-01-14T10:36:33.987Z',
+        testStationType: 'gvts',
+        testCode: 'aas',
+        vin: 'XMGDE02FS0H012303',
+        vrm: 'JY58FPP',
+        testStationPNumber: 'P99006',
+        testResult: 'fail',
+        testTypeName: 'Annual test',
+        vehicleType: 'psv',
+        testerName: 'Dorel',
+        testerStaffId: '2',
+        testResultId: '9',
+      };
+      expect(TEST_AVTIVITY).toEqual(EXPECTED_TEST_AVTIVITY);
+    });
+    // TODO: Edge Case = it(`GIVEN multiple test types WITH same date time EXPECT ???`, () => {})
   });
 });
