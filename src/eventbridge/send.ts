679a2443b80cc998efaa4b9c8547286df879fd33
Tom Webb <tomwebb559@gmail.com>
2022-01-11 09:38:01 +0000

    feat(cb2-1054): refactored sendEvents and added started unit tests

--- src/eventbridge/send.ts
+++ src/eventbridge/send.ts
@@ -3,43 +3,41 @@ import { EventEntry } from './EventEntry';
 import { Entries } from './Entries';
 import { SendResponse } from './SendResponse';
 import logger from '../observability/logger';
+import { TestActivity } from '../utils/testActivity';
 
 const eventbridge = new EventBridge();
-const sendEvents = async (schedules: string[]): Promise<SendResponse> => {
+const sendEvents = async (testActivity: TestActivity): Promise<SendResponse> => {
   logger.info('sendEvents starting');
-  logger.info(`${schedules.length} ${schedules.length === 1 ? 'event' : 'events'} ready to send to eventbridge.`);
+  logger.info('event ready to send to eventbridge.');
 
   const sendResponse: SendResponse = {
     SuccessCount: 0,
     FailCount: 0,
   };
 
-  for (let i = 0; i < schedules.length; i++) {
-    const entry: EventEntry = {
-      Source: process.env.AWS_EVENT_BUS_SOURCE,
-      // eslint-disable-next-line security/detect-object-injection
-      Detail: `{ "schedule": "${JSON.stringify(schedules[i])?.replace(/"/g, '\\"')}" }`,
-      DetailType: 'CVS ATF Daily Schedule',
-      EventBusName: process.env.AWS_EVENT_BUS_NAME,
-      Time: new Date(),
-    };
+  const entry: EventEntry = {
+    Source: process.env.AWS_EVENT_BUS_SOURCE,
+    // eslint-disable-next-line security/detect-object-injection
+    Detail: `{ "testResult": "${JSON.stringify(testActivity)}`,
+    DetailType: 'CVS ATF Test Results',
+    EventBusName: process.env.AWS_EVENT_BUS_NAME,
+    Time: new Date(),
+  };
 
-    const params: Entries = {
-      Entries: [],
-    };
-    params.Entries.push(entry);
+  const params: Entries = {
+    Entries: [],
+  };
+  params.Entries.push(entry);
 
-    try {
-      logger.debug(`event about to be sent: ${JSON.stringify(params)}`);
-      // TODO Make the putEvents run in parallel?
-      // eslint-disable-next-line no-await-in-loop
-      const result = await eventbridge.putEvents(params).promise();
-      logger.info(`${result.Entries.length} ${result.Entries.length === 1 ? 'event' : 'events'} sent to eventbridge.`);
-      sendResponse.SuccessCount++;
-    } catch (error) {
-      logger.error('', error);
-      sendResponse.FailCount++;
-    }
+  try {
+    logger.debug(`event about to be sent: ${JSON.stringify(params)}`);
+    // eslint-disable-next-line no-await-in-loop
+    await eventbridge.putEvents(params).promise();
+    logger.info('event sent to eventbridge.');
+    sendResponse.SuccessCount++;
+  } catch (error) {
+    logger.error('', error);
+    sendResponse.FailCount++;
   }
 
   logger.info('sendEvents ending');
---
+++ tests/unit/send.test.ts
@@ -0,0 +1,76 @@
+import { EventBridge, Request } from 'aws-sdk';
+import { mocked } from 'ts-jest/utils';
+import { PutEventsResponse, PutEventsRequest, PutEventsResultEntry } from 'aws-sdk/clients/eventbridge';
+import { sendEvents } from '../../src/eventbridge/send';
+import { SendResponse } from '../../src/eventbridge/SendResponse';
+import { TestActivity } from '../../src/utils/testActivity';
+
+jest.mock('aws-sdk', () => {
+  const mEventBridgeInstance = {
+    putEvents: jest.fn(),
+  };
+  const mRequestInstance = {
+    promise: jest.fn(),
+  };
+  const mEventBridge = jest.fn(() => mEventBridgeInstance);
+  const mRequest = jest.fn(() => mRequestInstance);
+
+  return { EventBridge: mEventBridge, Request: mRequest };
+});
+
+type PutEventsWithParams = (params: PutEventsRequest) => AWS.Request<PutEventsResponse, AWS.AWSError>;
+
+const mEventBridgeInstance = new EventBridge();
+const mResultInstance = new Request<PutEventsResponse, AWS.AWSError>(null, null);
+// eslint-disable-next-line @typescript-eslint/unbound-method
+mocked(mEventBridgeInstance.putEvents as PutEventsWithParams).mockImplementation(
+  (params: PutEventsRequest): AWS.Request<PutEventsResponse, AWS.AWSError> => {
+    const mPutEventsResponse: PutEventsResponse = {
+      FailedEntryCount: 0,
+      Entries: Array<PutEventsResultEntry>(params.Entries.length),
+    };
+    if (params.Entries[0].Detail === '{ "testResult": "{"noOfAxles":-1}') {
+      mResultInstance.promise = jest.fn().mockReturnValue(Promise.reject(new Error('Oh no!')));
+    } else {
+      mResultInstance.promise = jest.fn().mockReturnValue(Promise.resolve(mPutEventsResponse));
+    }
+    return mResultInstance;
+  },
+);
+
+describe('Send events', () => {
+  describe('Events sent', () => {
+    it('GIVEN one event to send WHEN sent THEN one event is returned.', async () => {
+      const mTestResult: TestActivity = createTestResult();
+      const mSendResponse: SendResponse = { SuccessCount: 1, FailCount: 0 };
+      await expect(sendEvents(mTestResult)).resolves.toEqual(mSendResponse);
+    });
+
+    it('GIVEN an issue with eventbridge WHEN 1 event is sent and 1 fails THEN the failure is in the response.', async () => {
+      const errorTestResult: TestActivity = createTestResult(-1);
+      const mSendResponse: SendResponse = { SuccessCount: 0, FailCount: 1 };
+      await expect(sendEvents(errorTestResult)).resolves.toEqual(mSendResponse);
+    });
+  });
+});
+
+function createTestResult(axels?: number): TestActivity {
+  const activityEvent: TestActivity = {
+    noOfAxles: axels || 0,
+    testTypeStartTimestamp: '',
+    testTypeEndTimestamp: '',
+    testStationType: '',
+    testCode: '',
+    vin: '',
+    vrm: '',
+    testStationPNumber: '',
+    testResult: '',
+    certificateNumber: '',
+    testTypeName: '',
+    vehicleType: '',
+    testerName: '',
+    testerStaffId: '',
+    testResultId: '',
+  };
+  return activityEvent;
+}
