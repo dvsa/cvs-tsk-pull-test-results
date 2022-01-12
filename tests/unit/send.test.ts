d4ed7a3b38cbaa4270a807cc0d0ec564d5ad90a2
Aaron Downing <aaron.downing@bjss.com>
2022-01-12 10:57:09 +0000

    feat(cb2-1058): add unit test coverage for handler, logger and send files

--- tests/unit/send.test.ts
+++ tests/unit/send.test.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { EventBridge, Request } from 'aws-sdk';
 import { mocked } from 'ts-jest/utils';
 import { PutEventsResponse, PutEventsRequest, PutEventsResultEntry } from 'aws-sdk/clients/eventbridge';
+import { EOL } from 'os';
 import { sendEvents } from '../../src/eventbridge/send';
 import { SendResponse } from '../../src/eventbridge/SendResponse';
 import { TestActivity } from '../../src/utils/testActivity';
@@ -29,7 +32,7 @@ mocked(mEventBridgeInstance.putEvents as PutEventsWithParams).mockImplementation
       FailedEntryCount: 0,
       Entries: Array<PutEventsResultEntry>(params.Entries.length),
     };
-    if (params.Entries[0].Detail === '{ "testResult": "{\\"noOfAxles\\":-1,\\"testTypeStartTimestamp\\":\\"\\",\\"testTypeEndTimestamp\\":\\"\\",\\"testStationType\\":\\"\\",\\"testCode\\":\\"\\",\\"vin\\":\\"\\",\\"vrm\\":\\"\\",\\"testStationPNumber\\":\\"\\",\\"testResult\\":\\"\\",\\"certificateNumber\\":\\"\\",\\"testTypeName\\":\\"\\",\\"vehicleType\\":\\"\\",\\"testerName\\":\\"\\",\\"testerStaffId\\":\\"\\",\\"testResultId\\":\\"\\"}" }') {
+    if (params.Entries[0].Detail === '{ "testResult": "{\\"noOfAxles\\":-1,\\"testTypeStartTimestamp\\":\\"\\",\\"testTypeEndTimestamp\\":\\"A\\",\\"testStationType\\":\\"\\",\\"testCode\\":\\"\\",\\"vin\\":\\"\\",\\"vrm\\":\\"\\",\\"testStationPNumber\\":\\"\\",\\"testResult\\":\\"\\",\\"certificateNumber\\":\\"\\",\\"testTypeName\\":\\"\\",\\"vehicleType\\":\\"\\",\\"testerName\\":\\"\\",\\"testerStaffId\\":\\"\\",\\"testResultId\\":\\"\\"}" }') {
       mResultInstance.promise = jest.fn().mockReturnValue(Promise.reject(new Error('Oh no!')));
     } else {
       mResultInstance.promise = jest.fn().mockReturnValue(Promise.resolve(mPutEventsResponse));
@@ -41,31 +44,38 @@ mocked(mEventBridgeInstance.putEvents as PutEventsWithParams).mockImplementation
 describe('Send events', () => {
   describe('Events sent', () => {
     it('GIVEN one event to send WHEN sent THEN one event is returned.', async () => {
-      const mTestResult: TestActivity[] = Array<TestActivity>(1);
+      const mTestResult: TestActivity[] = [createTestResult(0, 'A Real Date!')];
       const mSendResponse: SendResponse = { SuccessCount: 1, FailCount: 0 };
       await expect(sendEvents(mTestResult)).resolves.toEqual(mSendResponse);
     });
 
     it('GIVEN two events to send WHEN sent THEN two events are returned.', async () => {
-      const mTestResult: TestActivity[] = Array<TestActivity>(2);
+      const mTestResult: TestActivity[] = [createTestResult(0, 'A Real Date!'), createTestResult(0, 'Another real Date!')];
       const mSendResponse: SendResponse = { SuccessCount: 2, FailCount: 0 };
       await expect(sendEvents(mTestResult)).resolves.toEqual(mSendResponse);
     });
 
+    it('GIVEN event WHEN test that has not been completed THEN log info message', async () => {
+      // @ts-ignore
+      const consoleSpy = jest.spyOn(console._stdout, 'write');
+      const mTestResult: TestActivity[] = [createTestResult(0)];
+      await sendEvents(mTestResult);
+      expect(consoleSpy).toHaveBeenCalledWith(`info: Event not sent as not completed { ID:  }${EOL}`);
+    });
+
     it('GIVEN an issue with eventbridge WHEN 6 events are sent and 1 fails THEN the failure is in the response.', async () => {
-      const mTestResult: TestActivity[] = Array<TestActivity>(6);
-      mTestResult[0] = createTestResult(-1);
-      const mSendResponse: SendResponse = { SuccessCount: 5, FailCount: 1 };
+      const mTestResult: TestActivity[] = [createTestResult(0, 'A Real Date!'), createTestResult(0, 'A Real Date!'), createTestResult(0, 'A Real Date!'), createTestResult(0, 'A Real Date!'), createTestResult(-1, 'A')];
+      const mSendResponse: SendResponse = { SuccessCount: 4, FailCount: 1 };
       await expect(sendEvents(mTestResult)).resolves.toEqual(mSendResponse);
     });
   });
 });
 
-function createTestResult(axels?: number): TestActivity {
+function createTestResult(axels?: number, testTypeEndTimestamp?: string): TestActivity {
   const activityEvent: TestActivity = {
     noOfAxles: axels || 0,
     testTypeStartTimestamp: '',
-    testTypeEndTimestamp: '',
+    testTypeEndTimestamp: testTypeEndTimestamp || '',
     testStationType: '',
     testCode: '',
     vin: '',
