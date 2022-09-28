fc20f882ebd25ac53b5c78ed64e4866d17037799
Ian Hodges <ian.hodges@bjss.com>
2022-09-28 16:00:51 +0100

    CB2-3168 Add extra logging of results sent to eventbridge
    * feat(cb2-3168): remove cancelled status filter
    * feat(cb2-3168): remove test end timestamp filter and improve logging
    * feat(cb2-3168): amend test descriptions

--- tests/unit/send.test.ts
+++ tests/unit/send.test.ts
@@ -3,7 +3,6 @@
 import { EventBridge, Request } from 'aws-sdk';
 import { mocked } from 'ts-jest/utils';
 import { PutEventsResponse, PutEventsRequest, PutEventsResultEntry } from 'aws-sdk/clients/eventbridge';
-import { EOL } from 'os';
 import { sendEvents } from '../../src/eventbridge/send';
 import { SendResponse } from '../../src/eventbridge/SendResponse';
 import { TestActivity } from '../../src/utils/testActivity';
@@ -25,69 +24,98 @@ type PutEventsWithParams = (params: PutEventsRequest) => AWS.Request<PutEventsRe
 
 const mEventBridgeInstance = new EventBridge();
 const mResultInstance = new Request<PutEventsResponse, AWS.AWSError>(null, null);
+const mPutEventsResponse = { Entries: Array<PutEventsResultEntry>(1) };
 // eslint-disable-next-line @typescript-eslint/unbound-method
 mocked(mEventBridgeInstance.putEvents as PutEventsWithParams).mockImplementation(
   (params: PutEventsRequest): AWS.Request<PutEventsResponse, AWS.AWSError> => {
-    const mPutEventsResponse: PutEventsResponse = {
-      FailedEntryCount: 0,
-      Entries: Array<PutEventsResultEntry>(params.Entries.length),
-    };
-    if (params.Entries[0].Detail === '{ "testResult": "{\\"noOfAxles\\":-1,\\"testTypeStartTimestamp\\":\\"\\",\\"testTypeEndTimestamp\\":\\"A\\",\\"testStationType\\":\\"\\",\\"testCode\\":\\"\\",\\"vin\\":\\"\\",\\"vrm\\":\\"\\",\\"testStationPNumber\\":\\"\\",\\"testResult\\":\\"\\",\\"certificateNumber\\":\\"\\",\\"testTypeName\\":\\"\\",\\"vehicleType\\":\\"\\",\\"testerName\\":\\"\\",\\"testerStaffId\\":\\"\\",\\"testResultId\\":\\"\\"}" }') {
+    if (params.Entries[0].Detail.includes('\\"testResultId\\":\\"HandledError\\"')) {
+      mPutEventsResponse.Entries[0] = { ErrorMessage: 'Failed to process event.' };
+    } else {
+      mPutEventsResponse.Entries[0] = { EventId: '2468' };
+    }
+
+    if (params.Entries[0].Detail.includes('\\"testResultId\\":\\"UnhandledError\\"')) {
       mResultInstance.promise = jest.fn().mockReturnValue(Promise.reject(new Error('Oh no!')));
     } else {
       mResultInstance.promise = jest.fn().mockReturnValue(Promise.resolve(mPutEventsResponse));
     }
+
     return mResultInstance;
   },
 );
 
 describe('Send events', () => {
   describe('Events sent', () => {
+    // @ts-ignore
+    const consoleSpy = jest.spyOn(console._stdout, 'write');
+
     it('GIVEN one event to send WHEN sent THEN one event is returned.', async () => {
-      const mTestResult: TestActivity[] = [createTestResult(0, 'A Real Date!')];
+      const mTestResult: TestActivity[] = [createTestResult()];
       const mSendResponse: SendResponse = { SuccessCount: 1, FailCount: 0 };
       await expect(sendEvents(mTestResult)).resolves.toEqual(mSendResponse);
     });
 
-    it('GIVEN two events to send WHEN sent THEN two events are returned.', async () => {
+    it('GIVEN two events to send WHEN sent THEN two events are returned and two infos logged.', async () => {
       const mTestResult: TestActivity[] = [
-        createTestResult(0, 'A Real Date!'),
-        createTestResult(0, 'Another real Date!'),
+        createTestResult('Result1'),
+        createTestResult('Result2'),
       ];
       const mSendResponse: SendResponse = { SuccessCount: 2, FailCount: 0 };
       await expect(sendEvents(mTestResult)).resolves.toEqual(mSendResponse);
+      expect(consoleSpy).toHaveBeenCalledWith(
+        expect.stringContaining(
+          `info: Result sent to eventbridge (testResultId: 'Result1', vin: '${mTestResult[0].vin}')`,
+        ),
+      );
+      expect(consoleSpy).toHaveBeenCalledWith(
+        expect.stringContaining(
+          `info: Result sent to eventbridge (testResultId: 'Result2', vin: '${mTestResult[0].vin}')`,
+        ),
+      );
     });
 
-    it('GIVEN event WHEN test that has not been completed THEN log info message', async () => {
-      // @ts-ignore
-      const consoleSpy = jest.spyOn(console._stdout, 'write');
-      const mTestResult: TestActivity[] = [createTestResult(0)];
+    it('GIVEN an event WHEN eventbridge could not process it THEN log info message and error', async () => {
+      const mTestResult: TestActivity[] = [createTestResult('HandledError')];
       await sendEvents(mTestResult);
-      expect(consoleSpy).toHaveBeenCalledWith(`info: Event not sent as test is not completed { ID:  }${EOL}`);
+      expect(consoleSpy).toHaveBeenCalledWith(
+        expect.stringContaining(
+          `info: Failed to send result to eventbridge (testResultId: '${mTestResult[0].testResultId}', vin: '${mTestResult[0].vin}')`,
+        ),
+      );
+      expect(consoleSpy).toHaveBeenCalledWith(
+        expect.stringContaining(
+          'Error: Failed to process event.',
+        ),
+      );
     });
 
-    it('GIVEN an issue with eventbridge WHEN 6 events are sent and 1 fails THEN the failure is in the response.', async () => {
+    it('GIVEN an issue with eventbridge WHEN 5 events are sent and 1 fails THEN the failure is in the response and an error is logged.', async () => {
       const mTestResult: TestActivity[] = [
-        createTestResult(0, 'A Real Date!'),
-        createTestResult(0, 'A Real Date!'),
-        createTestResult(0, 'A Real Date!'),
-        createTestResult(0, 'A Real Date!'),
-        createTestResult(-1, 'A'),
+        createTestResult(),
+        createTestResult(),
+        createTestResult(),
+        createTestResult(),
+        createTestResult('UnhandledError'),
       ];
       const mSendResponse: SendResponse = { SuccessCount: 4, FailCount: 1 };
       await expect(sendEvents(mTestResult)).resolves.toEqual(mSendResponse);
+      expect(consoleSpy).toHaveBeenCalledWith(
+        expect.stringContaining(
+          'Error: Oh no!',
+        ),
+      );
     });
   });
 });
 
-function createTestResult(axels?: number, testTypeEndTimestamp?: string): TestActivity {
+function createTestResult(resultId?: string): TestActivity {
   const activityEvent: TestActivity = {
-    noOfAxles: axels || 0,
+    noOfAxles: 0,
     testTypeStartTimestamp: '',
-    testTypeEndTimestamp: testTypeEndTimestamp || '',
+    testTypeEndTimestamp: '',
     testStationType: '',
     testCode: '',
-    vin: '',
+    vin: '4321',
     vrm: '',
     testStationPNumber: '',
     testResult: '',
@@ -96,7 +124,7 @@ function createTestResult(axels?: number, testTypeEndTimestamp?: string): TestAc
     vehicleType: '',
     testerName: '',
     testerStaffId: '',
-    testResultId: '',
+    testResultId: resultId || '1234',
   };
   return activityEvent;
 }
