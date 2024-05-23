a738fb1844e5d42a4695437b51a099f5af28afdd
Shivangi Das <shivangi.das7@gmail.com>
2024-05-23 11:01:33 +0100

    fix(cb2-12122): check for undefined field

--- src/utils/extractAmendedBillableTestResults.ts
+++ src/utils/extractAmendedBillableTestResults.ts
@@ -20,27 +20,29 @@ export const extractAmendedBillableTestResults = (currentRecord: TestResultModel
     );
     logger.info(`current test code: ${JSON.stringify(currentTestType.testCode)}`);
     logger.info(`previous test type: ${JSON.stringify(previousTestType)}`);
-    logger.info(`previous record test code: ${JSON.stringify(previousTestType.testCode)}`);
-    const hasAnyFieldChanged = testResultValues.some((field) => currentRecord[field] !== previousRecord[field])
+    if (previousTestType) {
+      logger.info(`previous record test code: ${JSON.stringify(previousTestType.testCode)}`);
+      const hasAnyFieldChanged = testResultValues.some((field) => currentRecord[field] !== previousRecord[field])
       || currentTestType.testCode !== previousTestType.testCode;
 
-    if (!hasAnyFieldChanged) {
-      logger.info('No fields have changed which are relevant to billing');
-      return;
+      if (!hasAnyFieldChanged) {
+        logger.info('No fields have changed which are relevant to billing');
+        return;
+      }
+      fields.push({ fieldName: 'testCode', oldValue: previousTestType.testCode, newValue: currentTestType.testCode });
+      testResultValues.forEach((field) => fields.push({
+        fieldName: field === 'trailerId' ? 'vrm' : field,
+        oldValue: previousRecord[field],
+        newValue: currentRecord[field],
+      }));
+
+      logger.debug(`Fields changed for testResultId: ${currentRecord.testResultId}: ${JSON.stringify(fields)}`);
+
+      fieldsChanged.push({
+        reason: currentRecord.reasonForCreation,
+        fields,
+      });
     }
-    fields.push({ fieldName: 'testCode', oldValue: previousTestType.testCode, newValue: currentTestType.testCode });
-    testResultValues.forEach((field) => fields.push({
-      fieldName: field === 'trailerId' ? 'vrm' : field,
-      oldValue: previousRecord[field],
-      newValue: currentRecord[field],
-    }));
-
-    logger.debug(`Fields changed for testResultId: ${currentRecord.testResultId}: ${JSON.stringify(fields)}`);
-
-    fieldsChanged.push({
-      reason: currentRecord.reasonForCreation,
-      fields,
-    });
   });
 
   return fieldsChanged;
